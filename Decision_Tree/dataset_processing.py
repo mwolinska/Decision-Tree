484b9050d6f87b466e4fa4bc1145b975e04e112e
Marta <mswolinska@gmail.com>
2022-05-13 20:18:52 +0100

    Amend split_dataset function to return Dataset class
    Previously the function returned np.arrays as that was the type required by the DecisionTreeClassifier class. To follow the other changes a tuple of Dataset class is now returned instead.

--- Decision_Tree/dataset_processing.py
+++ Decision_Tree/dataset_processing.py
@@ -2,8 +2,10 @@ from typing import Tuple
 
 import numpy as np
 
+from Decision_Tree.tree_data_model import Dataset
 
-def split_dataset_using_shuffle(dataset: np.ndarray, dataset_ratio_for_training: float) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+
+def split_dataset_using_shuffle(dataset: np.ndarray, dataset_ratio_for_training: float) -> Tuple[Dataset, Dataset, Dataset]:
     ratio_for_training = 0.5 * (1 - dataset_ratio_for_training)
     dataset_size = len(dataset)
     first_split_index = int(dataset_size * dataset_ratio_for_training)
@@ -11,8 +13,8 @@ def split_dataset_using_shuffle(dataset: np.ndarray, dataset_ratio_for_training:
 
     np.random.shuffle(dataset)
 
-    training_dataset = dataset[:first_split_index]
-    test_dataset = dataset[first_split_index: second_split_index]
-    validate_dataset = dataset[second_split_index:]
+    training_dataset = Dataset.from_array(dataset[:first_split_index])
+    test_dataset = Dataset.from_array(dataset[first_split_index: second_split_index])
+    validate_dataset = Dataset.from_array(dataset[second_split_index:])
 
     return training_dataset, test_dataset, validate_dataset
